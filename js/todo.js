987a990fae4f6e3d6efd2e4552e2b68475886695
Tahara <tahara@studiom-web.net>
2018-08-12 22:23:06 +0900

    削除ボタンと削除機能追加

--- js/todo.js
+++ js/todo.js
@@ -7,10 +7,14 @@ function addItem() {
   todoItem.value = "";
   for (i in todoItems){
     let li = document.createElement('li');
-    li.innerHTML = `<input type="checkbox" onchange="myChange(event)">` + `<span>${ todoItems[i] }</span>`;
+    li.innerHTML = `<input type="checkbox" onchange="myChange(event)">` + `<span>${ todoItems[i] }</span>` + `<button onclick="deleteItem(event)">Delete</button>`;
     todoList.appendChild(li);
   }
 }
 function myChange(event){
   event.target.classList.toggle('done');
+}
+function deleteItem(event){
+  const del_parent = event.target.parentElement;
+  todoList.removeChild(del_parent);
 }

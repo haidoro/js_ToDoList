1b2ac6c57a7e7a3f0f840dde4a401436e6971130
Tahara <tahara@studiom-web.net>
2018-08-12 22:19:16 +0900

    入力値をリストで表示

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,13 @@
-
+const todoList = document.getElementById('todoList');
+const todoItem = document.getElementById('item');
+function addItem() {
+  const todoItems = [];
+  if(todoItem.value === "") return;
+  todoItems.push(todoItem.value);
+  todoItem.value = "";
+  for (i in todoItems){
+    let li = document.createElement('li');
+    li.innerHTML =  `<span>${ todoItems[i] }</span>`;
+    todoList.appendChild(li);
+  }
+}

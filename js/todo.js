93c83df83aa123df6349b3aeddaea5d060efde80
Tahara <hydoro-3181@itstudio.co>
2019-05-07 18:01:30 +0900

    JS版ToDoList

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,12 @@
 const todoList = document.getElementById('todoList');
 const todoItem = document.getElementById('item');
 function addItem() {
-  const todoItems = [];
   if(todoItem.value === "") return;
-  todoItems.push(todoItem.value);
+  const todoItems = todoItem.value;
   todoItem.value = "";
-  for (i in todoItems){
-    let li = document.createElement('li');
-    li.innerHTML = `<input type="checkbox" onchange="myChange(event)">` + `<span>${ todoItems[i] }</span>` + `<button onclick="deleteItem(event)">Delete</button>`;
-    todoList.appendChild(li);
-  }
+  let li = document.createElement('li');
+  li.innerHTML = `<input type="checkbox" onchange="myChange(event)">` + `<span>${ todoItems }</span>` + `<button onclick="deleteItem(event)">Delete</button>`;
+  todoList.appendChild(li);
 }
 function myChange(event){
   event.target.classList.toggle('done');
